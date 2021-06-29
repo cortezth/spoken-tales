729e3407a3dad95de96f2b484201723fb282406a
teeagu <72951239+teeagu@users.noreply.github.com>
2021-06-29 02:49:35 -0300

    implementado api voice rss

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
+import { useState } from 'react'
+
 function Home() {
+    const [text, setText] = useState('Text');
+
+    const key = '8df3f62b146248e8acb302b07905c720';
+    const url = `http://api.voicerss.org/?key=${key}&hl=en-us&src=${text}`;
+
+    function GetSpeech() {
+        const audio = new Audio(url);
+        audio.play();
+    }
+
     return (
         <div>
-            <h1>Spoken Tales</h1>
-            <h3>work in progress...</h3>
+            <input onChange={(e) => setText(e.target.value)} />
+            <button onClick={GetSpeech}>Play</button>
         </div>
     )
 }
